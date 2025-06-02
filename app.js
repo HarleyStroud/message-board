3ecbf0e7262a22370696ea6b00ab2ecb3c7b203f
HarleyStroud <stroudhd@gmail.com>
2025-06-02 09:08:35 -0400

    Remove duplicate server connection

--- app.js
+++ app.js
@@ -41,9 +41,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
 
 (async () => {
     try {
